cc573e3ffcd06f7a3fe11c09884306ae43932080
isaiasivg <isaias.vilchez.gil@gmail.com>
2021-03-24 13:13:35 +0100

    feat (Event) Passing Test 5

--- src/Event.cpp
+++ src/Event.cpp
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <iostream>
+#include <string.h>
 #include "Event.h"
 #include "DateTime.h"
 using namespace std;
@@ -17,10 +18,8 @@ Event::Event() {
 
 void Event::initDefault() {
     //const std::string EVENT_DEFAULT = (((std::string)"1971-01-01 00:00:00 UTC,view,")+EMPTY_FIELD+",,,,-1.000000,"+ EMPTY_FIELD+ ","+ EMPTY_FIELD);
-
-    DateTime time;
-    _dateTime = time;
     
+    DateTime objectTime();
     _type = "view";
     _prod_id = "00000000"; // NOTNULL
     _cat_id = "";
@@ -32,29 +31,10 @@ void Event::initDefault() {
 }
 
 Event::Event( string line) {
-    char separator = ',';
-    int c=0;
-    string values[9];
-    
-    for (size_t p=0, q=0; p!=line.npos;p=q){
-        values[c]=line.substr(p+(p!=0),(q=line.find(separator,p+1))-p-(p!=0));
-        c++;
-    }
-    
-    DateTime time(values[0]);
-    _dateTime = time;
-    _type=values[1];
-    _prod_id=values[2];
-    _cat_id=values[3];
-    _cat_cod=values[4];
-    _brand=values[5];
-    _price=stod(values[6]);
-    _user_id=values[7];
-    _session=values[8];
+    set(line);
 }
 
-DateTime Event::getDateTime()  {
-}
+DateTime Event::getDateTime()  { return _dateTime.to_string();}
 
 string Event::getType()  { return _type; }
 
@@ -73,42 +53,75 @@ string Event::getUserID()  { return _user_id; }
 string Event::getSession()  { return _session; }
 
 void Event::setDateTime( string  time) {
+    //DateTime _dateTime (time);
+    DateTime objectTime(time);
+    _dateTime= objectTime;
 }
 
-void Event::setType( std::string  type) {
-
+void Event::setType( std::string type) {
+    _type=type;
 }
 
 void Event::setProductID( std::string  prod_id) {
- }
+    if(prod_id=="")
+        _prod_id=EMPTY_FIELD;
+    
+    else _prod_id=prod_id;
+}
 
 void Event::setCategoryID( std::string  cat_id) {
+    _cat_id=cat_id;
 }
 
 void Event::setCategoryCode( std::string  cat_cod) {
+    _cat_cod=cat_cod;
 }
 
 void Event::setBrand( std::string  brand) {
+    _brand=brand;
 }
 
 void Event::setPrice(double price) {
-
+    if(price >= 0)
+        _price=price;
 }
 
 void Event::setUserID( std::string  user_id) {
-
+    if(user_id=="")
+        _user_id=EMPTY_FIELD;
+    
+    else _user_id=user_id;
 }
 
 void Event::setSession( std::string  session) {
-
+    if(session == "")
+        _session=EMPTY_FIELD;
+    
+    else _session=session;
 }
 
 void Event::set( std::string line) {
-
+    char separator = ',';
+    int c=0;
+    string values[9];
+    
+    for (size_t p=0, q=0; p!=line.npos;p=q){
+        values[c]=line.substr(p+(p!=0),(q=line.find(separator,p+1))-p-(p!=0));
+        c++;
+    }
+      
+    setDateTime(values[0]);
+    setType(values[1]);
+    setProductID(values[2]);
+    setCategoryID(values[3]);
+    setCategoryCode(values[4]);
+    setBrand(values[5]);
+    setPrice(stod(values[6]));
+    setUserID(values[7]);
+    setSession(values[8]);
 }
 
 bool Event::isEmpty()  {
-
 }
 
 string Event::to_string() const {
